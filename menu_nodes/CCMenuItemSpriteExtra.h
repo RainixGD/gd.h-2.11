a51eefffc32dcd51f9eb7669c7a1f39ed89e851f
RainixGD <gmdrainix@gmail.com>
2024-12-13 17:27:17 +0200

    CCMenuItemSpriteExtra update

--- menu_nodes/CCMenuItemSpriteExtra.h
+++ menu_nodes/CCMenuItemSpriteExtra.h
@@ -3,13 +3,27 @@
 
 #include <gd.h>
 
+enum class MenuAnimationType {
+    Scale = 0,
+    Move = 1,
+};
+
 namespace gd {
 	#pragma runtime_checks("s", off)
 	class CCMenuItemSpriteExtra : public cocos2d::CCMenuItemSprite {
 	protected:
-		float m_fUnknown;
-		float m_fUnknown2;
-		PAD(96);
+		float m_scaleMultiplier;
+		float m_baseScale;
+		bool m_animationEnabled;
+		bool m_colorEnabled;
+		float m_unknown;
+		std::string m_unknown2;
+		std::string m_unknown3;
+		float m_colorDip;
+		cocos2d::CCPoint m_destPosition;
+		cocos2d::CCPoint m_offset;
+		MenuAnimationType m_animationType;
+		cocos2d::CCPoint m_startPosition;
 		CCMenuItemSpriteExtra() {
 			reinterpret_cast<void*(__thiscall*)(CCMenuItemSpriteExtra*)>(base + 0x18db0)(this);
 		}
@@ -29,6 +43,34 @@ namespace gd {
 			return reinterpret_cast<void(__thiscall*)(CCMenuItemSpriteExtra*)>(
 				base + 0x19080
 				)(this);
+		}
+		bool init(
+			cocos2d::CCNode* sprite,
+			cocos2d::CCObject* target,
+			cocos2d::SEL_MenuHandler callback
+		){
+			return init(sprite, sprite, target, callback);
+		}
+		
+		bool init(
+			cocos2d::CCNode* normalImage,
+			cocos2d::CCNode* selectedImage,
+			cocos2d::CCObject* target,
+			cocos2d::SEL_MenuHandler callback
+		) {
+			if (!cocos2d::CCMenuItemSprite::initWithNormalSprite(
+				normalImage, selectedImage, nullptr, target, callback
+			)) return false;
+
+			this->setAnchorPoint({ .5f, .5f });
+			this->setContentSize(normalImage->getScaledContentSize());
+			normalImage->setPosition(m_obContentSize / 2);
+
+			m_baseScale = 1.f;
+			m_animationEnabled = true;
+			m_scaleMultiplier = 1.26f;
+
+			return true;
 		}
 	};
 	#pragma runtime_checks("s", restore)
